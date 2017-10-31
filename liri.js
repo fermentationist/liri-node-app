cf29f422f9f980b9938ea2dcb107009662a226c9
Dennis Hoges <fermentationist@gmail.com>
2017-10-31 01:37:57 -0500

    spotifyThisSong and myTweets functions completed.

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 const keys = require("./keys.js");
-const twitter = require("twitter");
+const Twitter = require("twitter");
 const Spotify = require("node-spotify-api");
 const request = require("request");
 //console.log("Twitter consumer key: ", keys.twitterKeys.consumer_key);
@@ -35,8 +35,34 @@ switch (command.toLowerCase()){
 }
 
 function myTweets(args){
+
+	var twitter = new Twitter({
+		consumer_key: keys.twitterKeys.consumer_key,
+		consumer_secret: keys.twitterKeys.consumer_secret,
+		access_token_key: keys.twitterKeys.access_token_key,
+		access_token_secret: keys.twitterKeys.access_token_secret
+	})
+
+	let screenName = args || "@StephenAtHome";
+
+	let params = {screen_name: screenName, count: 20}
+	twitter.get('statuses/user_timeline', params, function (error, tweets, response){
+		if (error){
+			throw error;
+		}
+
+		for (let i = 0; i < tweets.length ; i ++){
+			let created_at = tweets[i].created_at;
+			let text = tweets[i].text;
+			let tweet = {time: created_at, tweet: text}
+			displayResults(tweet, i + 1);
+		}
+
+
+	})
 	return console.log("myTweets", args);
 }
+
 
 function spotifyThisSong(args){
 
@@ -45,19 +71,45 @@ function spotifyThisSong(args){
 		secret: spotifySecret
 	});
 
-	spotify.search({ type: 'track', query: args, limit: 20}, function(err, data) {
+	spotify.search({ type: 'track', query: args, limit: 1}, function(err, data) {
   		if (err) {
     		return console.log('Error occurred: ' + err);
   		}
-		console.log("\n", data.tracks); 
+  		let results = data.tracks.items[0];
+		// console.log("\n", results); 
+		let artist = results.artists[0].name;
+		let name = results.name;
+		let preview_url = results.preview_url;
+		let album = results.album.name;
+		let trackInfo = {name: name, artist: artist, album: album, preview_link: preview_url};
+		console.log('trackInfo', trackInfo);
+		return displayResults(trackInfo, "Spotify results");
 	});
-	return;// console.log("spotifyThisSong", args);
 }
+
 
 function doWhatItSays(args){
 	return console.log("doWhatItSays", args);
 }
 
+
 function movieThis(args){
 	return console.log("movieThis", args);
 }
+
+
+function displayResults(obj, heading = ""){
+	console.log("\n=========================")
+	console.log(heading + "\n");
+	for (prop in obj){
+		console.log(prop,":", obj[prop]);
+	}
+	console.log("\n=========================\n")
+}
+
+
+
+
+
+
+
