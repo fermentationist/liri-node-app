6943a5815c9403b98a3358d795a95811e69fb705
Dennis Hoges <fermentationist@gmail.com>
2017-11-01 08:35:59 -0500

    Deleted and created new package.json and node_modules and re-installed dependencies.

--- liri.js
+++ liri.js
@@ -2,24 +2,12 @@ const keys = require("./keys.js");
 const Twitter = require("twitter");
 const Spotify = require("node-spotify-api");
 const request = require("request");
-//console.log("Twitter consumer key: ", keys.twitterKeys.consumer_key);
+const fs = require("file-system");
 const spotifyID = keys.spotifyKeys.clientID;
 const spotifySecret = keys.spotifyKeys.clientSecret;
 
-/*
-my-tweets
-
-spotify-this-song
-
-movie-this
-
-do-what-it-says
-*/
-
 let command = process.argv[2] || "";
 let args = process.argv.slice(3).join(" ");
-console.log('args =', args);
-console.log('command =', command);
 
 switch (command.toLowerCase()){
 	case "my-tweets":
@@ -36,73 +24,77 @@ switch (command.toLowerCase()){
 }
 
 function myTweets(args){
-
 	var twitter = new Twitter({
 		consumer_key: keys.twitterKeys.consumer_key,
 		consumer_secret: keys.twitterKeys.consumer_secret,
 		access_token_key: keys.twitterKeys.access_token_key,
 		access_token_secret: keys.twitterKeys.access_token_secret
 	})
-
 	let screenName = args || "@StephenAtHome";
-
 	let params = {screen_name: screenName, count: 20}
 	twitter.get('statuses/user_timeline', params, function (error, tweets, response){
 		if (error){
 			throw error;
 		}
-
 		for (let i = 0; i < tweets.length ; i ++){
 			let created_at = tweets[i].created_at;
 			let text = tweets[i].text;
 			let tweet = {time: created_at, tweet: text}
 			displayResults(tweet, i + 1);
 		}
-
-
 	})
-	return console.log("myTweets", args);
 }
 
 
 function spotifyThisSong(args){
-
 	var spotify = new Spotify ({
 		id: spotifyID,
 		secret: spotifySecret
 	});
-
 	spotify.search({ type: 'track', query: args, limit: 1}, function(err, data) {
 		if (err) {
 			return console.log('Error occurred: ' + err);
 		}
 		let results = data.tracks.items[0];
-		// console.log("\n", results); 
 		let artist = results.artists[0].name;
 		let name = results.name;
 		let preview_url = results.preview_url;
 		let album = results.album.name;
 		let trackInfo = {name: name, artist: artist, album: album, preview_link: preview_url};
-		console.log('trackInfo', trackInfo);
 		return displayResults(trackInfo, "Spotify results");
 	});
 }
 
 
 function doWhatItSays(args){
-	return console.log("doWhatItSays", args);
+	fs.readFile("./random.txt", "utf8", function (error, content){
+		if (error) {
+			console.log("error", error);
+		}
+		let argArray = content.split(",");
+		let command = argArray[0];
+		console.log('command', command);
+		let args = argArray[1];
+		console.log('args', args);
+		switch (command.toLowerCase()){
+			case "my-tweets":
+				return myTweets(args);
+			case "spotify-this-song":
+				console.log("spotifyThisSong() called.");""
+				return spotifyThisSong(args);
+			case "movie-this":
+				return movieThis(args);
+			case "do-what-it-says":
+				return doWhatItSays(args);
+			default: 
+				console.log("\nInvalid input. \nAvailable commands: my-tweets, spotify-this-song, \nmovie-this, do-what-it-says\n");
+				return 
+		}
+	})
 }
 
 
 function movieThis(args){
-	// * Title of the movie.
-	// * Year the movie came out.
-	// * IMDB Rating of the movie.
-	// * Rotten Tomatoes Rating of the movie.
-	// * Country where the movie was produced.
-	// * Language of the movie.
-	// * Plot of the movie.
-	// * Actors in the movie.
 	const omdbKey = keys.omdbKey;
 	let queryUrl = "http://www.omdbapi.com/?apikey=" + omdbKey + "&s=" + args;
 	request (queryUrl, function (error, response, body){
@@ -117,9 +109,7 @@ function movieThis(args){
 			if(error){
 				console.log("error", error);
 			}
-			console.log("body = ", typeof body);
 			let movieData = JSON.parse(body);
-			console.log('movieData', movieData);
 			let title = movieData.Title;
 			let year = movieData.Year;
 			let imdbRating = movieData.imdbRating;
@@ -140,7 +130,6 @@ function movieThis(args){
 
 		});
 	});
-	return console.log ("movieThis", args);
 }
 
 
